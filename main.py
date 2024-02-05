59d8a08a49ac8302c5c1d48d181d29ae77c8709d
mfink351 <m.fink351@gmail.com>
2024-02-05 22:53:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+
+print("Welcome to the tip calculator.")
+total = input("What was the total bill? $")
+num_people = input("How many people to split the bill? ")
+
+while True:
+  tip_percentage = input("What percentage tip would you like to give? 10, 12, or 15? ")
+  if tip_percentage == "10" or tip_percentage == "12" or tip_percentage == "15":
+    break
+  else:
+    print("Please enter a valid tip percentage.")
+
+total_with_tip = float(total) * (1 + int(tip_percentage)/100)
+
+print(f"Each person should pay: ${round(total_with_tip / int(num_people), 2)}")
